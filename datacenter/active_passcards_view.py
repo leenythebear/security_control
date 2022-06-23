59d8369c1476ce0db2897ab7d07dd5822628baaa
leenythebear <Leeny@inbox.ru>
2022-06-23 18:57:17 +0700

    make a function to get active passcards

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -4,10 +4,9 @@ from django.shortcuts import render
 
 
 def active_passcards_view(request):
-    # Программируем здесь
+    active_passcards = Passcard.objects.filter(is_active=True)
 
-    all_passcards = Passcard.objects.all()
     context = {
-        'active_passcards': all_passcards,  # люди с активными пропусками
+        'active_passcards': active_passcards,  # люди с активными пропусками
     }
     return render(request, 'active_passcards.html', context)
